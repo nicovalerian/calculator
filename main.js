1541c82f8db4f675668aaeebeb2e98c24492deee
Nico <nicovalerian2@gmail.com>
2024-04-13 15:42:24 +0700

    Apply DRY code

--- main.js
+++ main.js
@@ -1,32 +1,17 @@
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    if (b === 0) {
-        return "Cannot divide by zero";
-    }
-
-    return a / b;
-}
-
 function operate(operator, a, b) {
     switch (operator) {
         case "+":
-            return add(a, b);
+            return a + b;
         case "-":
-            return subtract(a, b);
+            return a - b;
         case "*":
-            return multiply(a, b);
+            return a * b;
         case "/":
-            return divide(a, b);
+            if (b === 0) {
+                return "Cannot divide by zero";
+            }
+            return a / b;
+        default:
+            return "Invalid operator";
     }
 }
