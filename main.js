43b58e95bc5238049930d6a0241c32c8d2ca4f9c
Nico <nicovalerian2@gmail.com>
2024-04-13 13:52:57 +0700

    Add evaluation function

--- main.js
+++ main.js
@@ -17,3 +17,16 @@ function divide(a, b) {
 
     return a / b;
 }
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
