97851e1b2fa0d0a3504a1de2ecb19b30a12ffa9b
Nico <nicovalerian2@gmail.com>
2024-04-13 15:56:09 +0700

    Add event listeners

--- main.js
+++ main.js
@@ -15,3 +15,43 @@ function operate(operator, a, b) {
             return "Invalid operator";
     }
 }
+
+const operandButtons = document.querySelectorAll(".operand-btn");
+const operatorButtons = document.querySelectorAll('.operator-btn');
+const equalsButton = document.querySelector('.equals-btn');
+const clearButton = document.querySelector('.clear-btn');
+const signButton = document.querySelector('.sign-btn');
+const percentButton = document.querySelector('.percent-btn');
+const decimalButton = document.querySelector('.decimal-btn');
+
+operandButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(button.value);
+    })
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(button.value);
+    })
+});
+
+equalsButton.addEventListener("click", () => {
+    console.log("=");
+});
+
+clearButton.addEventListener("click", () => {
+    console.log("AC");
+});
+
+signButton.addEventListener("click", () => {
+    console.log("+/-");
+});
+
+percentButton.addEventListener("click", () => {
+    console.log("%");
+});
+
+decimalButton.addEventListener("click", () => {
+    console.log(".");
+});
