29ba74db74e1dc75fa645214325a1fb4b79223d1
Nico <nicovalerian2@gmail.com>
2024-04-13 13:36:35 +0700

    Add basic arithmetic functions

---
+++ main.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Cannot divide by zero";
+    }
+
+    return a / b;
+}
